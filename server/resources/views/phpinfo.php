07694656d433f0d40f25d890547a76149b9c2576
Igor Smyrnov <smyrnovai@gmail.com>
2017-12-02 00:28:34 +0100

    Angular+firebase init

--- server/resources/views/phpinfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Igor
- * Date: 25.11.2017
- * Time: 20:54
- */
-
-phpinfo();
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBh1-Pfuspa0KUWy9LwUWrZBhfH5LIuCIk",
+    authDomain: "l-tracker-65ab0.firebaseapp.com",
+    databaseURL: "https://l-tracker-65ab0.firebaseio.com",
+    projectId: "l-tracker-65ab0",
+    storageBucket: "l-tracker-65ab0.appspot.com",
+    messagingSenderId: "192587989604"
+  }
+};
